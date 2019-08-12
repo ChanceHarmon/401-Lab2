e6c779c2e6d8b2e0d2efb81c5ccdcd53bb9f17ec
Chance Harmon <henrikillsit@gmail.com>
2019-08-12 16:15:40 -0700

    tring to fix

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict';
 
-const validator = require('./lib/validator.js');
+const validator = require('./lib/validator');
+
+console.log(validator.isValid({ input: 13, rules: 'true' }));
+console.log(validator.isValid({ input: -5, rules: 'false' }));
+console.log(validator.isValid({ input: 1, rules: 'true' }));
