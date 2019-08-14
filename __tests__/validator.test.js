6f87e2817124c0967f57f0ad46fcb45e9ac2e4a3
Chance Harmon <henrikillsit@gmail.com>
2019-08-13 19:02:50 -0700

    Lab 1, so so

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -2,65 +2,42 @@
 
 const validator = require('../lib/validator.js');
 
-describe('validator module performs basic validation of', () => {
-	// TODO: Make this series of tests less repetitive ... DRY it out
 
-	it('strings', () => {
-		let str = 'yes';
-		let num = 1;
-		let arr = [ 'a' ];
-		let obj = { x: 'y' };
-		let func = () => {};
-		let bool = false;
-		expect(validator.isString(str)).toBeTruthy();
-		expect(validator.isString(num)).toBeFalsy();
-		expect(validator.isString(arr)).toBeFalsy();
-		expect(validator.isString(obj)).toBeFalsy();
-		expect(validator.isString(func)).toBeFalsy();
-		expect(validator.isString(bool)).toBeFalsy();
-	});
-
-	it('numbers', () => {
-		expect(true).toBeFalsy();
-	});
-
-	it('arrays', () => {
-		expect(true).toBeFalsy();
-	});
-
-	it('objects', () => {
-		expect(true).toBeFalsy();
-	});
-
-	it('booleans', () => {
-		expect(true).toBeFalsy();
-	});
-
-	it('functions', () => {
-		expect(true).toBeFalsy();
+describe('#validator', () => {
+	describe('#isLessThanTen', () => {
+		test('Test normal input', () => {
+			expect(validator.isLessThanTen(1)).toEqual(true);
+		})
 	});
 });
 
-describe('validator module performs complex validations', () => {
-	it('validates the presence of required object properties at any level', () => {
-		// i.e. does person.hair.color exist and have a good value, not just person.hair
-		expect(true).toBeFalsy();
-	});
+describe('#isString', () => {
+	test('Test if input is a function', () => {
+		expect(validator.isString('alex', 'juliet', 'dog')).toEqual(true);
+	})
+})
 
-	it('validates the proper types of object properties', () => {
-		// i.e. person.name must be a string, etc.
-		expect(true).toBeFalsy();
-	});
 
-	it('validates the types of values contained in an array', () => {
-		// i.e. an array of all strings or numbers
-		expect(true).toBeFalsy();
-	});
+describe('#isObjectValid', () => {
+	test('regular cases', () => {
+		const schema = {
+			fields: {
+				id: { type: 'string' },
+				age: { type: 'number' },
+				favoriteToys: { type: 'objects' }
+			},
+		};
 
-	it('validates a value array against an approved list', () => {
-		// i.e. a string might only be allowed to be "yes" or "no"
-		expect(true).toBeFalsy();
-	});
+		const data1 = { id: 'conan', age: 12, favoriteToys: {} }
+		expect(validator.isObjectValid(
+			data1, schema
+		)).toEqual(true);
 
-	// TODO: Cover so, so many more cases
-});
+		const data2 = { id: 462, favoriteToys: [] }
+
+		expect(validator.isObjectValid(
+			data2, schema
+		)).toEqual(false);
+
+	})
+})
--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const validator = require('./lib/validator');
 
-console.log(validator.isValid({ input: 13, rules: 'true' }));
-console.log(validator.isValid({ input: -5, rules: 'false' }));
-console.log(validator.isValid({ input: 1, rules: 'true' }));
+console.log(validator.isLessThanTen({ input: 9, rules: 'true' }));
+console.log(validator.isLessThanTen({ input: -5, rules: 'true' }));
+
