56995f9afbc101dece0340a59b1913e0b8a4b5cf
Chance Harmon <henrikillsit@gmail.com>
2019-08-14 12:46:56 -0700

    little more

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,43 +1,57 @@
 'use strict';
 
-const validator = require('../lib/validator.js');
+const Validator = require('../lib/validator.js');
+const validator = new Validator;
 
 
-describe('#validator', () => {
-	describe('#isLessThanTen', () => {
-		test('Test normal input', () => {
-			expect(validator.isLessThanTen(1)).toEqual(true);
-		})
-	});
+
+describe('#isNumber', () => {
+	test('Test normal input', () => {
+		expect(validator.isNumber(1)).toEqual(true);
+	})
 });
 
+
 describe('#isString', () => {
-	test('Test if input is a function', () => {
+	test('Test if input is a string', () => {
 		expect(validator.isString('alex', 'juliet', 'dog')).toEqual(true);
 	})
-})
+});
 
-
-describe('#isObjectValid', () => {
-	test('regular cases', () => {
-		const schema = {
-			fields: {
-				id: { type: 'string' },
-				age: { type: 'number' },
-				favoriteToys: { type: 'objects' }
-			},
-		};
-
-		const data1 = { id: 'conan', age: 12, favoriteToys: {} }
-		expect(validator.isObjectValid(
-			data1, schema
-		)).toEqual(true);
-
-		const data2 = { id: 462, favoriteToys: [] }
-
-		expect(validator.isObjectValid(
-			data2, schema
-		)).toEqual(false);
-
+describe('#isArray', () => {
+	test('Test if input is an array', () => {
+		expect(validator.isArray(['alex', 'juliet', 'dog'])).toEqual([]);
 	})
 })
+
+describe('#isObject', () => {
+	test('Test if input is an object', () => {
+		let obj = alex;
+		expect(validator.isObject({ obj })).toEqual(true);
+	})
+})
+
+
+// describe('#isObjectValid', () => {
+// 	test('regular cases', () => {
+// 		const schema = {
+// 			fields: {
+// 				id: { type: 'string' },
+// 				age: { type: 'number' },
+// 				favoriteToys: { type: 'objects' }
+// 			}
+// 		};
+
+		// const data1 = { id: 'conan', age: 12, favoriteToys: {} }
+		// expect(validator.isObjectValid(
+		// 	data1, schema
+		// )).toEqual(true);
+
+		// const data2 = { id: 462, favoriteToys: [] }
+
+		// expect(validator.isObjectValid(
+		// 	data2, schema
+		// )).toEqual(false);
+
+// 	})
+// })
