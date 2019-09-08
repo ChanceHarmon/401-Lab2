b1375922637752342e2a3bfece1271abc543e70d
Chance Harmon <henrikillsit@gmail.com>
2019-09-08 11:55:21 -0700

    almost

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const validator = require('./lib/validator');
-
-console.log(validator.isLessThanTen({ input: 9, rules: 'true' }));
-console.log(validator.isLessThanTen({ input: -5, rules: 'true' }));
-
---
+++ validator/validator.js
@@ -0,0 +1,66 @@
+'use strict';
+class Validator {
+
+	constructor(schema) {
+		this.schema = schema;
+	}
+
+	static isNegative(input) { return input < 0; }
+
+	static isString(input) { return typeof input === 'string'; }
+
+	static isNumber(input) { return typeof input === 'number'; }
+
+	static isArray(input) { return Array.isArray(input); }
+
+	static isObject(input) { return typeof input === 'object' && !Array.isArray(input); }
+
+	static isBoolean(input) { return typeof input === 'boolean'; }
+
+	static isFunction(input) { return typeof input === 'function'; }
+
+	static isTruthy(input) { return Boolean(input) }
+
+	static isNumberValid(number, rule) {
+		if (!Validator.isNumber(number)) {
+			return false;
+		}
+
+		if (rule === 'positive') {
+			return number > 0;
+		}
+
+		if (rule === 'negative') {
+			return number < 0;
+		}
+
+		if (rule === 'zero') {
+			return number === 0;
+		}
+
+
+		return false;
+	}
+
+	isValid(data) {
+		if (!Validator.isObject(data) || !Validator.isObject(this.schema)) {
+			return false;
+		}
+		if (!this.schema.hasOwnProperty('fields')) {
+			return false;
+		}
+		for (let field of Object.keys(this.schema.fields)) {
+			if (!data.hasOwnProperty(field)) {
+				return false;
+			}
+			const expectedType = this.schema.fields[field].type;
+			const fieldType = Validator.isArray(data[field]) ? 'array' : typeof data[field];
+			if (fieldType !== expectedType) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+module.exports = Validator;
